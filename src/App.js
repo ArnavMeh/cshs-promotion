645094804015a851ade065983b6110f46d198200
ArnavMeh <55332253+ArnavMeh@users.noreply.github.com>
2020-12-24 19:49:37 -0500

    el navbaro es completidad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import React from 'react'
-
+import "antd/dist/antd.css";
+import "./App.css"
 function App() {
   return (
     <div className="App">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,67 @@
 import React from 'react';
-
 import logo from "../images/logo.png";
+import hseapps from  '../images/hseapps.png'
+import hsecourses from '../images/courses.png'
+import hseclubs from '../images/clubs.png'
+import {Button, Menu} from 'antd';
+
+
+const block = {
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center",
+    minWidth:"150px"
+}
+
 
 
 const Navbar = () => {
-    const block = {
-         
-        display:"flex",
-        justifyContent:"spaceBetween",
-        alignItems:"center"
-    }
-
+    
     return (
-        <div style={{display:"flex", justifyContent:"space-between", background:"#aaaaaa", margin:"-10px", alignItems:"center", padding:"25px 50px 25px 50px"}}>
-            {[1,2,3,4,5].map(() => 
-                <div style={block}>
-                    <img src={logo} style={{height:"60px", marginRight:"10px"}}></img>
-                    <div style={{color:"white", fontWeight:"600", fontSize:"20px"}}>HSE CS</div>
-                </div>
-            )}
+        <div style={{
+            display:"flex", 
+            justifyContent:"space-between", 
+            background:"#bbbbbb", 
+            alignItems:"center", 
+            padding:"20px 40px 20px 40px",
+            margin:"0 -5px 0 -5px"
+        }}>
+            
+            <div style={block}>
+                <img src={logo} style={{height:"45px", filter: "brightness(0) invert(1) drop-shadow(0 2px #b5daff)"}}/>
+                <div style={{color:"white", fontWeight:"500", fontSize:"25px"}}>HSE CS</div>
+            </div>
+
+            <div style={{...block, height:"30px"}}>
+                <Menu mode="horizontal" style={{backgroundColor:"transparent", color: "white", borderBottom:"none", fontSize:"16px"}}>
+                    <Menu.Item style={{color:"white"}}>
+                        <img src={hsecourses} style={{width:"20px", marginBottom:"4px"}}/>
+                        &nbsp; Courses
+                    </Menu.Item>
+                    <Menu.Item style={{color:"white"}}>
+                        <a href="https://hseapps.org" style={{color:"white"}} target="_blank">
+                            <img src={hseapps} style={{width:"20px", marginBottom:"4px", filter: "brightness(0) invert(1) drop-shadow(0 2px #b5daff)"}}/>
+                            &nbsp; HSE Apps
+                        </a>
+                    </Menu.Item>
+                    <Menu.Item style={{color:"white"}}>
+                        <img src={hseclubs} style={{width:"20px", marginBottom:"4px"}}/>
+                        &nbsp; Clubs
+                    </Menu.Item>
+                    <Menu.Item style={{color:"white"}}>
+                        <span style={{display: "inline-block", transform: "rotate(45deg) scale(1.2, .9)", filter: "drop-shadow(0 2px #b5daff)", color: "white"}}>
+                            ?
+                        </span>
+                        &nbsp; FAQ
+                    </Menu.Item>
+                </Menu>
+            </div>
+
+            <div style={{...block, justifyContent: "flex-end"}}>
+                <Button ghost shape="round">
+                    Contact Us
+                </Button>
+            </div>
         </div>
     )
 }
